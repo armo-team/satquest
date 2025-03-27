ede8f45b89fde9f02eac77d12590b7eb45259f35
moakilodash <torkimohammad4@gmail.com>
2025-03-27 16:00:39 +0100

    updated the config files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { svelteTesting } from '@testing-library/svelte/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/postcss';
+import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
@@ -15,28 +14,10 @@ export default defineConfig({
 		}
 	},
 	test: {
-		workspace: [
-			{
-				extends: './vite.config.js',
-				plugins: [svelteTesting()],
-				test: {
-					name: 'client',
-					environment: 'jsdom',
-					clearMocks: true,
-					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
-					exclude: ['src/lib/server/**'],
-					setupFiles: ['./vitest-setup-client.js']
-				}
-			},
-			{
-				extends: './vite.config.js',
-				test: {
-					name: 'server',
-					environment: 'node',
-					include: ['src/**/*.{test,spec}.{js,ts}'],
-					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
-				}
-			}
-		]
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+		environment: 'jsdom',
+		deps: {
+			inline: ['@sveltejs/kit']
+		}
 	}
 });
